7162127341b2693923da2b09755c074968f89c67
Cory Manson <cory.manson@act.gov.au>
2021-07-22 20:43:26 +1000

    Update email settings

--- api/send-mailmessage/index.js
+++ api/send-mailmessage/index.js
@@ -1,9 +1,10 @@
 module.exports = async function (context, req) {
     if (req.body.email) {
         var email = {
-            from: {
-                email: req.body.email
-            },
+            // from: {
+            //     email: req.body.email
+            // },
+            to: ["#{EMAIL_TO}#", req.body.email],
             subject: "New " + req.body.type + " form submission from: " + req.body.name,
             content: [{
                 type: 'text/plain',
