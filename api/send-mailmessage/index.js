133fe25d28f5360a313e93570239a59899079813
Esther-Manson <87787766+Esther-Manson@users.noreply.github.com>
2021-07-22 20:23:06 +1000

    Update index.js

--- api/send-mailmessage/index.js
+++ api/send-mailmessage/index.js
@@ -1,9 +1,9 @@
 module.exports = async function (context, req) {
     if (req.body.email) {
         var email = {
-            from: {
-                email: req.body.email
-            },
+            // from: {
+            //     email: req.body.email
+            // },
             subject: "New " + req.body.type + " form submission from: " + req.body.name,
             content: [{
                 type: 'text/plain',
